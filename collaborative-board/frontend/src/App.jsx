779e7e1c3039812a1f038d52c26c9c8145797655
komi_yes <danielpalacios04@gmail.com>
2026-03-09 19:47:42 -0500

    fix(frontend): Update API URL for production deployment

--- collaborative-board/frontend/src/App.jsx
+++ collaborative-board/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import p5 from 'p5';
 import './App.css';
 
-const API_URL = 'https://lab-arsw-5.onrender.com/api';//'http://localhost:8080/api';
+const API_URL = 'https://3.144.222.91:8080/api';//'https://lab-arsw-5.onrender.com/api';//'http://localhost:8080/api';
 
 const USER_COLORS = [
   '#E63946', // red
